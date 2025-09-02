aabb30aeef973ce0fc6cfc6c9ae4599723dc5d6a
Gansons <gansonsraivis@gmail.com>
2025-09-02 12:07:24 +0300

    Ieviests cikls

--- src/LVT/Programma.java
+++ src/LVT/Programma.java
@@ -3,13 +3,17 @@ package LVT;
 import java.util.Random;
 
 public class Programma {
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		Random rand = new Random();
-		int skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		int skaitlis;
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
+		
 	}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 	}
 
 }
