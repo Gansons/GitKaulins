2c4f2d63c13adcf6ad0363b4cfbea2de7a4ffe36
Gansons <gansonsraivis@gmail.com>
2025-09-10 08:16:02 +0300

    Otras lekcijas sakums

--- src/LVT/Programma.java
+++ src/LVT/Programma.java
@@ -1,14 +1,21 @@
 package LVT;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Programma {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
+		scan.close();	
 	}
 
 }
