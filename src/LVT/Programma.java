e4d2f189fa8343aa71af782886e0d085b8be9cd6
Gansons <gansonsraivis@gmail.com>
2025-09-16 09:12:32 +0300

    Pievienots paziņojums

--- src/LVT/Programma.java
+++ src/LVT/Programma.java
@@ -7,6 +7,7 @@ public class Programma {
 
 
 	public static void main(String[] args) {
+		System.out.println("Esi sveicināgs manā metamā kauliņa programmā!");
 		int skaitlis, reizes;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes mest kauliņu?");
